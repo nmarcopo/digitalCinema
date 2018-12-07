ff0492c0cc0fb5c95374d6b45624b43773800d7b
Nicholas Marcopoli <nmarcopo@nd.edu>
2018-12-07 04:32:21 -0500

    finshing touches

--- appServer/js/main.js
+++
@@ -1,7 +0,0 @@
-// Set up files with NavBar and Modal Inserts
-$(function () {
-    var path = window.location.pathname;
-    var page = path.split("/").pop();
-    var pageName = page.split(".")[0];
-    $("#navBarInsert").load("res/nav.html");
-});
